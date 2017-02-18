baeebb0378fef1b76ba15ca16e46444864dbef9c
chloroplast <41893204@qq.com>
2017-02-18 21:36:21 +0800

    更新同步配置文件脚本

--- sync-config.sh
+++ sync-config.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
+function pingEtcd {
+	#尝试ping 2次, 不输出错误信息
+	ping -c 2 etcd.etcd-ha > /dev/null 2>&1
+	echo $?
+}
+
+function syncConfig {
+	confd -confdir="$1" -onetime -backend etcd -node http://etcd.etcd-ha:2379
+}
+
+#尝试5次
+checkTimes=0
+while [ `pingEtcd` -ne 0 ]
+do
+	sleep 1
+	let checkTimes++
+	
+	if [ $checkTimes -gt 5 ]
+	then
+		echo 'check connection fail!'
+		exit 1
+	fi
+done
+
+#检测没有问题后同步配置文件
 if [ $1 == "sandbox" ]
 then
-	confd -confdir="conf/sandbox" -onetime -backend etcd -node http://etcd.etcd-ha:2379
-	echo "conf sandbox done"
+	syncConfig "conf/sandbox"
 fi
+
 if [ $1 == "production" ]
 then
-	confd -confdir="conf/production" -onetime -backend etcd -node http://etcd.etcd-ha:2379
-	echo "conf production done"
+	syncConfig "conf/production"
 fi
